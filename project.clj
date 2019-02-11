efda8f8287c13d251f078972814d1dc95be99c95
gallimorej <jeff.gallimore@excella.com>
2019-02-10 22:05:12 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject thankser "0.1.0-SNAPSHOT"
+(defproject proj1 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://thankser.herokuapp.com"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-http "3.9.1"]
                  [org.clojure/data.json "0.2.6"]
@@ -13,10 +13,10 @@
                  [environ "1.0.2"]
                  [hiccup "1.0.5"]]
 
-  :main ^:skip-aot thankser.core
   :min-lein-version "2.0.0"
-  :target-path "target/%s"
-  :uberjar-name "thankser.jar"
+  :plugins [[environ/environ.lein "0.3.1"]]
+  ;:hooks [environ.leiningen.hooks]
+  :uberjar-name "proj1.jar"
   :profiles {:production {:env {:production true}}
              :dev        {:plugins [[com.jakemccrary/lein-test-refresh "LATEST"]
                                     [lein-ring "0.12.4"]
@@ -24,5 +24,6 @@
              :uberjar    {:aot :all}}
   :ring {:handler       thankser.web/handler
          :auto-reload?  true
-         :auto-refresh? true})
+         :auto-refresh? true}
 
+  :main thankser.core)
