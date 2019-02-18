71647e0c37909c7d11e3ca84a7dfa69ae41ddba1
gallimorej <jeff.gallimore@excella.com>
2019-02-18 08:49:44 -0500

    Change language help text

--- src/thankser/web.clj
+++ src/thankser/web.clj
@@ -51,7 +51,7 @@
 
 (defn get-languages-page-body
   []
-  (str "I know how to say thank you in the following languages:\n"
+  (str "Thankser knows how to say thank you in the following languages:\n"
        (strip (pr-str (map name (ty/get-languages))) "()\"")))
 
 (defn get-thanks-page-body [slack-text]
